554f081e2a9fee4feae72dee189e7d22cb35792b
joeylemon <joeyclemon@gmail.com>
2021-01-02 03:28:03 +0000

    update ytdl

--- cmds/bday.js
+++ cmds/bday.js
@@ -3,19 +3,28 @@ const utils = require('../utils.js')
 const constants = require('../constants.js')
 const config = require('../config.js')
 
-const ids = config.get("discord_ids")
-const users = Object.keys(ids).filter(key => ids[key].birthday !== undefined).map(key => ids[key])
-for (const user of users) {
-    const bday = new Date(user.birthday)
-    bday.setFullYear(new Date().getFullYear())
-    var j = schedule.scheduleJob(bday, function () {
-        constants.client.channels.fetch(config.get("discord_channel_id"))
-            .then(channel => {
-                channel.send(`Happy birthday ${user.real_name}, you little ${utils.getRandomInsult()}`, { files: ["https://i.imgur.com/KKgL784.gif"] })
-            })
-            .catch(err => console.error(`Cannot find discord_channel_id: ${err}`))
-    })
+function scheduleBirthdayMessages() {
+    const ids = config.get("discord_ids")
+
+    const users = Object.keys(ids).
+        filter(key => ids[key].birthday !== undefined).
+        map(key => ids[key])
+
+    for (const user of users) {
+        const bday = new Date(user.birthday)
+        bday.setFullYear(new Date().getFullYear())
+
+        schedule.scheduleJob(bday, () => {
+            constants.client.channels.fetch(config.get("discord_channel_id"))
+                .then(channel => {
+                    channel.send(`Happy birthday ${user.real_name}, you little ${utils.getRandomInsult()}`, { files: ["https://i.imgur.com/KKgL784.gif"] })
+                })
+                .catch(err => console.error(`Cannot send birthday message: ${err}`))
+        })
+    }
 }
+
+scheduleBirthdayMessages()
 
 module.exports = (msg) => {
     const args = msg.content.split(" ")
